7ed6db0386e812de8d256d19a246083486b030e0
Janson Chew <42407833+haichew@users.noreply.github.com>
2022-03-27 15:03:37 +0800

    Create DonationsMarket.sol

---
+++ contracts/DonationsMarket.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.0;
+import "./Token.sol";
+import "eth-heap/contracts/Heap.sol";
+
+contract DonationsMarket {
+
+    Token tokenContract;
+    
+     constructor(Token tokenAddress) public {
+        tokenContract = tokenAddress;
+        mapping(uint256 => uint256) tokenList;
+
+    }
+
+    event listedEvent(uint256 tokenId);
+    event unlistedEvent(uint256 tokenId);
+
+
+    //list a token for sale using token id
+    function list(uint256 tokenId) public {
+       require(msg.sender == tokenContract.getOwner(tokenId));
+       tokenList[tokenId] = tokenContract.getTokenAmt(tokenId);
+       emit listedEvent(id);
+    }
+
+    function unlist(uint256 tokenId) public {
+       require(msg.sender == tokenContract.getOwner(tokenId));
+       tokenList[tokenId] = 0;
+       emit unlistedEvent(id);
+  }
+
+    // if amount of token left <1, unlist token
+    function checkRemainder(uint256 tokenId) public {
+
+        if(tokenContract.getTokenAmt(tokenId) < 1){
+            unlist(tokenId);
+        }
+ }
+
+    // for recipients to input requests to be sent to token contract
+    function tokenRequest(uint256 tokenId, uint256 amt, uint256 deadline) public {
+        return tokenContract.addRequest(tokenId, msg.sender, amt, deadline);
+    }
+
+    //matching algorithm
+    
+}
